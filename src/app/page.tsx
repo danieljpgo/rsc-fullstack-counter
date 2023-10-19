1b7432c3a9ed222bfaa3fbbf2986ebc4c1ba2a7e
Daniel Jorge <djpgonzaga@gmail.com>
2023-10-18 21:12:46 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  const count = 0;
+  return (
+    <main>
+      <form action="">
+        <button type="submit">{count}</button>
+      </form>
+    </main>
+  );
+}
